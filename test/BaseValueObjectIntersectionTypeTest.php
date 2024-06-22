adf676bb8acf40f0f605ba3533b938f1b418b726
akitanabe <tanabe@determaind.biz>
2024-06-22 03:46:41 +0000

    feat: 交差型のテスト

---
+++ test/BaseValueObjectIntersectionTypeTest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Akitanabe\PhpValueObject\BaseValueObject;
+
+
+interface ITypeA
+{
+}
+
+class TypeB implements ITypeA
+{
+    public const TYPE = 'B';
+}
+
+class TypeC
+{
+    public const TYPE = 'C';
+}
+
+class IntersectionTypeValue extends BaseValueObject
+{
+    public (ITypeA&TypeB)|string $AandBorString;
+    public bool|float $floatOrBool;
+}
+
+class BaseValueObjectIntersectionTypeTest extends TestCase
+{
+    #[Test]
+    public function intersectionTypeObject()
+    {
+        $intersectionTypeValue = new IntersectionTypeValue(
+            AandBorString: new TypeB(),
+            floatOrBool: false,
+        );
+
+        $this->assertSame(TypeB::class, $intersectionTypeValue->AandBorString::class);
+        $this->assertSame(false, $intersectionTypeValue->floatOrBool);
+
+        $intersectionTypeValue = new IntersectionTypeValue(
+            AandBorString: "string",
+            floatOrBool: 0.01,
+        );
+
+        $this->assertSame("string", $intersectionTypeValue->AandBorString);
+        $this->assertSame(0.01, $intersectionTypeValue->floatOrBool);
+    }
+
+    public static function intersectionProperyWithInvalidTypeProvider(): array
+    {
+        return [
+            ["AandBorString", new TypeC()],
+            ["AandBorString", 1],
+            ["floatOrBool", "0.01"],
+            ["floatOrBool", 1],
+        ];
+    }
+
+    #[Test]
+    #[DataProvider("intersectionProperyWithInvalidTypeProvider")]
+    public function interserctionProperyWithInvalidType($prop, $val)
+    {
+        $this->expectException(TypeError::class);
+        new IntersectionTypeValue(...[$prop => $val]);
+    }
+}
