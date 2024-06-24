9c9d3b579eff4733a9ffb09ed1ce62ff6e68ddc6
akitanabe <tanabe@determaind.biz>
2024-06-24 14:43:22 +0000

    doc: コメントを追加

--- src/Attributes/AllowUninitializedProperty.php
+++ src/Attributes/AllowUninitializedProperty.php
@@ -7,6 +7,9 @@ namespace Akitanabe\PhpValueObject\Attributes;
 use Attribute;
 use Akitanabe\PhpValueObject\Options\Allowable;
 
+/**
+ * 初期化していないプロパティを許可する
+ */
 #[Attribute(Attribute::TARGET_CLASS)]
 final class AllowUninitializedProperty implements Allowable
 {
