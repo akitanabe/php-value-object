ff46acaf7e81c9180763e254d5494f9c2c122c4a
akitanabe <tanabe@determaind.biz>
2025-03-01 03:39:00 +0000

    feat: $flagsを追加

--- src/Helpers/AttributeHelper.php
+++ src/Helpers/AttributeHelper.php
@@ -10,20 +10,23 @@ use ReflectionClassConstant;
 use ReflectionFunctionAbstract;
 use ReflectionObject;
 use ReflectionParameter;
+use ReflectionProperty;
 
 final class AttributeHelper
 {
     /**
+     * @template C of object
      * @template T of object
-     * @param ReflectionClass<T>|ReflectionClassConstant|ReflectionFunctionAbstract|ReflectionObject|ReflectionParameter $reflection
+     * @param ReflectionClass<C>|ReflectionClassConstant|ReflectionFunctionAbstract|ReflectionObject|ReflectionParameter|ReflectionProperty $reflection
      * @param class-string<T> $attributeName
      *
      * @return ?ReflectionAttribute<T>
      */
     public static function getAttribute(
-        ReflectionClass|ReflectionClassConstant|ReflectionFunctionAbstract|ReflectionObject|ReflectionParameter $reflection,
+        ReflectionClass|ReflectionClassConstant|ReflectionFunctionAbstract|ReflectionObject|ReflectionParameter|ReflectionProperty $reflection,
         string $attributeName,
+        int $flags = 0,
     ): ?ReflectionAttribute {
-        return $reflection->getAttributes($attributeName)[0] ?? null;
+        return $reflection->getAttributes($attributeName, $flags)[0] ?? null;
     }
 }
