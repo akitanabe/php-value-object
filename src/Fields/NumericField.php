9b8fb8031ce98b1a9794a25772d83cdb7e2619bb
akitanabe <tanabe@determaind.biz>
2025-03-14 10:31:13 +0000

    feat: 数値比較オプションを持つNumericFieldを実装
    - gt: より大きい
    - lt: より小さい
    - ge: 以上
    - le: 以下
    
    の4つの比較オプションを提供し、より柔軟な数値の検証を可能にする

---
+++ src/Fields/NumericField.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpValueObject\Fields;
+
+use Attribute;
+use Closure;
+use Override;
+use PhpValueObject\Exceptions\ValidationException;
+use PhpValueObject\Support\PropertyOperator;
+
+/**
+ * NumericField
+ * @phpstan-import-type default_factory from BaseField
+ */
+#[Attribute(Attribute::TARGET_PROPERTY)]
+final class NumericField extends BaseField
+{
+    /**
+     *
+     * @param ?default_factory $defaultFactory
+     * @param ?string $alias
+     * @param float|int|null $gt より大きい
+     * @param float|int|null $lt より小さい
+     * @param float|int|null $ge 以上
+     * @param float|int|null $le 以下
+     */
+    public function __construct(
+        string|array|Closure|null $defaultFactory = null,
+        string|null $alias = null,
+        private float|int|null $gt = null,
+        private float|int|null $lt = null,
+        private float|int|null $ge = null,
+        private float|int|null $le = null,
+    ) {
+        parent::__construct($defaultFactory, $alias);
+    }
+
+    /**
+     *
+     * @throws ValidationException
+     */
+    #[Override]
+    public function validate(PropertyOperator $propertyOperator): void
+    {
+        $value = $propertyOperator->value;
+
+        $invalidMessage = 'Invalid Field Value';
+        if (!is_numeric($value)) {
+            throw new ValidationException("{$invalidMessage}. Must be numeric");
+        }
+
+        $numericValue = (float) $value;
+
+        // gt (>) の検証
+        if ($this->gt !== null && $numericValue <= $this->gt) {
+            throw new ValidationException(
+                "{$invalidMessage}. Must be greater than {$this->gt}"
+            );
+        }
+
+        // lt (<) の検証
+        if ($this->lt !== null && $numericValue >= $this->lt) {
+            throw new ValidationException(
+                "{$invalidMessage}. Must be less than {$this->lt}"
+            );
+        }
+
+        // ge (>=) の検証
+        if ($this->ge !== null && $numericValue < $this->ge) {
+            throw new ValidationException(
+                "{$invalidMessage}. Must be greater than or equal to {$this->ge}"
+            );
+        }
+
+        // le (<=) の検証
+        if ($this->le !== null && $numericValue > $this->le) {
+            throw new ValidationException(
+                "{$invalidMessage}. Must be less than or equal to {$this->le}"
+            );
+        }
+    }
+}
---
+++ test/Fields/NumericFieldTest.php
@@ -0,0 +1,138 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpValueObject\Test\Fields;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use PhpValueObject\Exceptions\ValidationException;
+use PhpValueObject\Fields\NumericField;
+use PhpValueObject\Support\InputData;
+use PhpValueObject\Support\PropertyOperator;
+use ReflectionProperty;
+
+class NumericFieldValidateTestClass
+{
+    public float $prop;
+}
+
+class NumericFieldTest extends TestCase
+{
+    /**
+     * @return array<string, array{
+     *   value: mixed,
+     *   expectException: bool,
+     *   gt?: float|int,
+     *   lt?: float|int,
+     *   ge?: float|int,
+     *   le?: float|int
+     * }>
+     */
+    public static function validateDataProvider(): array
+    {
+        return [
+            '整数の場合は検証が成功する' => [
+                'value' => 123,
+                'expectException' => false,
+            ],
+            '小数の場合は検証が成功する' => [
+                'value' => 123.45,
+                'expectException' => false,
+            ],
+            '文字列として表現された数値の場合は検証が成功する' => [
+                'value' => "123.45",
+                'expectException' => false,
+            ],
+            '数値以外の場合は例外が発生する' => [
+                'value' => 'abc',
+                'expectException' => true,
+            ],
+            'gtで指定した値より大きい場合は検証が成功する' => [
+                'value' => 15,
+                'expectException' => false,
+                'gt' => 10,
+            ],
+            'gtで指定した値以下の場合は例外が発生する' => [
+                'value' => 5,
+                'expectException' => true,
+                'gt' => 10,
+            ],
+            'ltで指定した値より小さい場合は検証が成功する' => [
+                'value' => 15,
+                'expectException' => false,
+                'lt' => 20,
+            ],
+            'ltで指定した値以上の場合は例外が発生する' => [
+                'value' => 25,
+                'expectException' => true,
+                'lt' => 20,
+            ],
+            'geで指定した値以上の場合は検証が成功する' => [
+                'value' => 20,
+                'expectException' => false,
+                'ge' => 20,
+            ],
+            'geで指定した値より小さい場合は例外が発生する' => [
+                'value' => 15,
+                'expectException' => true,
+                'ge' => 20,
+            ],
+            'leで指定した値以下の場合は検証が成功する' => [
+                'value' => 20,
+                'expectException' => false,
+                'le' => 20,
+            ],
+            'leで指定した値より大きい場合は例外が発生する' => [
+                'value' => 25,
+                'expectException' => true,
+                'le' => 20,
+            ],
+            'ゼロ値の場合は検証が成功する' => [
+                'value' => 0,
+                'expectException' => false,
+            ],
+        ];
+    }
+
+    /**
+     * @param mixed $value
+     * @param bool $expectException
+     * @param float|int|null $gt
+     * @param float|int|null $lt
+     * @param float|int|null $ge
+     * @param float|int|null $le
+     */
+    #[Test]
+    #[DataProvider('validateDataProvider')]
+    public function testValidate(
+        mixed $value,
+        bool $expectException,
+        float|int|null $gt = null,
+        float|int|null $lt = null,
+        float|int|null $ge = null,
+        float|int|null $le = null,
+    ): void {
+        $field = new NumericField(
+            gt: $gt,
+            lt: $lt,
+            ge: $ge,
+            le: $le,
+        );
+
+        $refProperty = new ReflectionProperty(NumericFieldValidateTestClass::class, 'prop');
+        $inputData = new InputData(['prop' => $value]);
+
+        $propertyOperator = new PropertyOperator($refProperty, $inputData, $field);
+
+        if ($expectException) {
+            $this->expectException(ValidationException::class);
+        }
+
+        $field->validate($propertyOperator);
+
+        // @phpstan-ignore method.alreadyNarrowedType (例外が発生しなければテストは成功)
+        $this->assertTrue(true);
+    }
+}
