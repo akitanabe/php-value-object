2c95337ddbe80d93912555e992a9c10765f7c0cb
akitanabe <tanabe@determaind.biz>
2025-04-12 10:06:23 +0000

    feat: ValidatorFunctionWrapHandlerクラスを追加し、バリデータのラッピング機能を実装

---
+++ src/Support/ValidatorFunctionWrapHandler.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpValueObject\Support;
+
+use ArrayIterator;
+use PhpValueObject\Exceptions\ValidationException;
+use InvalidArgumentException;
+use PhpValueObject\Validators\Validatorable;
+
+final class ValidatorFunctionWrapHandler
+{
+    private readonly int $currentIndex;
+
+    /**
+     * @param ArrayIterator<int,Validatorable> $validators
+     */
+    public function __construct(
+        private ArrayIterator $validators,
+    ) {
+        $validators->next();
+        $this->currentIndex = $validators->key() > -1 ? $validators->key() : -1;
+    }
+
+    /**
+     * @param mixed $value
+     *
+     * @return mixed
+     * @throws ValidationException
+     */
+    public function __invoke(mixed $value): mixed
+    {
+        if ($this->currentIndex < 0) {
+            return $value;
+        }
+
+        $this->validators->seek($this->currentIndex);
+
+        $validator = $this->validators->current();
+
+        $nextHandler = new self($this->validators);
+
+        return match ($validator->getMode()) {
+            'before', 'after', 'field' => $nextHandler($validator->validate($value)),
+            default => throw new InvalidArgumentException('Invalid validator mode'),
+        };
+    }
+}
--- src/Validators/Validatorable.php
+++ src/Validators/Validatorable.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace PhpValueObject\Validators;
 
 use PhpValueObject\Exceptions\ValidationException;
+use PhpValueObject\Support\ValidatorFunctionWrapHandler;
 
 /**
  * バリデーション処理のインターフェース
  *
- * @phpstan-type validator_mode 'before'|'after'
+ * @phpstan-type validator_mode 'before'|'after'|'field'
  * @phpstan-type validator_callable callable-string|class-string|array{string|object, string}|\Closure
  */
 interface Validatorable
@@ -18,10 +19,11 @@ interface Validatorable
      * バリデーション処理を実行する
      *
      * @param mixed $value 検証する値
+     * @param ValidatorFunctionWrapHandler|null $handler 内部バリデーション処理をラップするハンドラ
      * @return mixed バリデーション後の値
      * @throws ValidationException バリデーションに失敗した場合
      */
-    public function validate(mixed $value): mixed;
+    public function validate(mixed $value, ?ValidatorFunctionWrapHandler $handler = null): mixed;
 
     /**
      * バリデーション処理の実行順序を取得する
