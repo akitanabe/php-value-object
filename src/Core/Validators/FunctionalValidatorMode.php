8e91eacd1454c0b6b12657572e333872278c203b
akitanabe <tanabe@determaind.biz>
2025-04-29 09:29:45 +0000

    feat: FunctionalValidatorMode Enum を追加し、バリデーションの実行タイミングを定義

---
+++ src/Core/Validators/FunctionalValidatorMode.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpValueObject\Core\Validators;
+
+/**
+ * バリデーションの実行タイミングや方法を示す Enum
+ */
+enum FunctionalValidatorMode
+{
+    /**
+     * FunctionBeforeValidator を利用する場合
+     */
+    case BEFORE;
+
+    /**
+     * FunctionAfterValidator を利用する場合
+     */
+    case AFTER;
+
+    /**
+     * FunctionWrapValidator を利用する場合
+     */
+    case WRAP;
+
+    /**
+     * FunctionPlainValidator を利用する場合
+     */
+    case PLAIN;
+}
