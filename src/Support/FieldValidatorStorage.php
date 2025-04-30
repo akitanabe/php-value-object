8a4f4f2c704c7bf968b42113676ff9f20e91e6fb
akitanabe <tanabe@determaind.biz>
2025-04-30 13:25:21 +0000

    feat: FieldValidatorStorageクラスを追加し、ReflectionPropertyとFieldValidatorの関連付けを管理する機能を実装

---
+++ src/Support/FieldValidatorStorage.php
@@ -0,0 +1,118 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpValueObject\Support;
+
+use InvalidArgumentException;
+use PhpValueObject\Helpers\AttributeHelper;
+use PhpValueObject\Validators\FieldValidator;
+use ReflectionClass;
+use ReflectionProperty;
+use SplObjectStorage;
+use ReflectionException;
+
+/**
+ * ReflectionPropertyとFieldValidatorの関連付けを管理するストレージクラス
+ * @extends SplObjectStorage<ReflectionProperty, FieldValidator[]>
+ */
+final class FieldValidatorStorage extends SplObjectStorage
+{
+    /**
+     * nameプロパティのハッシュ値を取得する
+     * @param ReflectionProperty $property
+     */
+    public function getHash(object $property): string
+    {
+        return $property->name;
+    }
+
+    /**
+     * ReflectionClassからFieldValidatorStorageインスタンスを生成する
+     *
+     * @template T of object
+     * @param ReflectionClass<T> $refClass 対象クラスのReflection
+     * @return self
+     * @throws InvalidArgumentException メソッドがstaticでない場合
+     */
+    public static function createFromClass(ReflectionClass $refClass): self
+    {
+        $instance = new self();
+        $className = $refClass->getName();
+
+        foreach ($refClass->getMethods() as $refMethod) {
+            $fieldValidators = AttributeHelper::getAttributeInstances($refMethod, FieldValidator::class);
+
+            if (empty($fieldValidators)) {
+                continue;
+            }
+
+            $methodName = $refMethod->getName();
+
+            // staticメソッドであることを確認
+            if (!$refMethod->isStatic()) {
+                throw new InvalidArgumentException(
+                    "Method {$className}::{$methodName} must be static for use with FieldValidator",
+                );
+            }
+
+            $validatorCallable = [$className, $methodName];
+
+            // 各FieldValidatorアトリビュートに対して処理
+            foreach ($fieldValidators as $fieldValidator) {
+                $fieldName = $fieldValidator->field;
+
+                // 指定されたフィールド名に対応するReflectionPropertyを探す
+                try {
+                    $refProperty = $refClass->getProperty($fieldName);
+                } catch (ReflectionException $e) {
+                    throw new InvalidArgumentException(
+                        "Property '{$fieldName}' referenced in {$className}::{$methodName} does not exist",
+                    );
+                }
+
+                // FieldValidatorにcallableをセット
+                $fieldValidator->setCallable($validatorCallable);
+
+                // ReflectionPropertyとFieldValidatorを関連付ける
+                $instance->addValidator($refProperty, $fieldValidator);
+            }
+        }
+
+        return $instance;
+    }
+
+    /**
+     * ReflectionPropertyに対応するバリデータを追加する
+     *
+     * @param ReflectionProperty $property 対象プロパティのReflection
+     * @param FieldValidator $validator 追加するFieldValidatorインスタンス
+     * @return void
+     */
+    public function addValidator(ReflectionProperty $property, FieldValidator $validator): void
+    {
+        $validators = [];
+
+        if ($this->offsetExists($property)) {
+            $validators = $this->offsetGet($property);
+        }
+
+        $validators[] = $validator;
+        $this->offsetSet($property, $validators);
+    }
+
+    /**
+     * 指定されたReflectionPropertyに対応するFieldValidatorの配列を取得する
+     *
+     * @param ReflectionProperty $property 対象プロパティのReflection
+     * @return FieldValidator[] 対応するFieldValidatorの配列（存在しない場合は空配列）
+     */
+    public function getValidatorsForProperty(ReflectionProperty $property): array
+    {
+        if ($this->offsetExists($property) === false) {
+            return [];
+        }
+
+        return $this->offsetGet($property);
+    }
+}
---
+++ test/Support/FieldValidatorStorageTest.php
@@ -0,0 +1,202 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpValueObject\Test\Support;
+
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+use PhpValueObject\Validators\FieldValidator;
+use PhpValueObject\Support\FieldValidatorStorage;
+use PhpValueObject\Validators\FunctionalValidatorMode;
+use ReflectionClass;
+use ReflectionProperty;
+
+class FieldValidatorStorageTest extends TestCase
+{
+    /**
+     * クラスからFieldValidatorStorageインスタンスを生成できることをテスト
+     * - クラスからストレージを作成する
+     * - nameプロパティに1つのバリデータが設定されていることを確認
+     * - ageプロパティに2つのバリデータが設定されていることを確認
+     */
+    #[Test]
+    public function canCreateFieldValidatorStorageFromClass(): void
+    {
+        // Arrange
+        $refClass = new ReflectionClass(TestClassWithValidators::class);
+
+        // Act
+        $storage = FieldValidatorStorage::createFromClass($refClass);
+
+        // Assert
+        $this->assertInstanceOf(FieldValidatorStorage::class, $storage);
+
+        // name プロパティのバリデータが正しく設定されていることを確認
+        $nameProperty = $refClass->getProperty('name');
+        $validators = $storage->getValidatorsForProperty($nameProperty);
+        $this->assertCount(1, $validators);
+        $this->assertInstanceOf(FieldValidator::class, $validators[0]);
+        $this->assertSame('name', $validators[0]->field);
+
+        // age プロパティのバリデータが正しく設定されていることを確認
+        $ageProperty = $refClass->getProperty('age');
+        $validators = $storage->getValidatorsForProperty($ageProperty);
+        $this->assertCount(2, $validators);
+        $this->assertInstanceOf(FieldValidator::class, $validators[0]);
+        $this->assertInstanceOf(FieldValidator::class, $validators[1]);
+        $this->assertSame('age', $validators[0]->field);
+        $this->assertSame('age', $validators[1]->field);
+    }
+
+    /**
+     * 非staticメソッドにFieldValidatorアトリビュートが付いている場合に例外が発生することをテスト
+     * - staticでないメソッドにアトリビュートが設定されたクラスを使用
+     * - createFromClassメソッド呼び出し時に例外が発生することを確認
+     */
+    #[Test]
+    public function throwsExceptionWhenNonStaticMethodHasAttribute(): void
+    {
+        // Arrange
+        $refClass = new ReflectionClass(TestClassWithNonStaticValidator::class);
+
+        // Act & Assert
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage(
+            'Method PhpValueObject\Test\Support\TestClassWithNonStaticValidator::validateName must be static for use with FieldValidator',
+        );
+        FieldValidatorStorage::createFromClass($refClass);
+    }
+
+    /**
+     * 存在しないプロパティ名がFieldValidatorアトリビュートで指定された場合に例外が発生することをテスト
+     * - 存在しないプロパティ名を参照しているクラスを使用
+     * - createFromClassメソッド呼び出し時に例外が発生することを確認
+     */
+    #[Test]
+    public function throwsExceptionWhenInvalidPropertyNameSpecified(): void
+    {
+        // Arrange
+        $refClass = new ReflectionClass(TestClassWithInvalidPropertyName::class);
+
+        // Act & Assert
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage(
+            "Property 'nonExistentProperty' referenced in PhpValueObject\Test\Support\TestClassWithInvalidPropertyName::validateNonExistent does not exist",
+        );
+        FieldValidatorStorage::createFromClass($refClass);
+    }
+
+    /**
+     * addValidatorメソッドでプロパティにバリデータを追加できることをテスト
+     * - 既存のプロパティに新しいバリデータを追加
+     * - バリデータが正しく追加されていることを確認
+     */
+    #[Test]
+    public function canAddValidatorToProperty(): void
+    {
+        // Arrange
+        $refClass = new ReflectionClass(TestClassWithValidators::class);
+        $storage = FieldValidatorStorage::createFromClass($refClass);
+        $nameProperty = $refClass->getProperty('name');
+        $validator = new FieldValidator('name', FunctionalValidatorMode::BEFORE);
+        $validator->setCallable([TestClassWithValidators::class, 'additionalValidator']);
+
+        // Act
+        $storage->addValidator($nameProperty, $validator);
+
+        // Assert
+        $validators = $storage->getValidatorsForProperty($nameProperty);
+        $this->assertCount(2, $validators);
+        $this->assertSame($validator, $validators[1]);
+    }
+
+    /**
+     * 存在しないプロパティに対してgetValidatorsForPropertyを呼び出すと空配列が返されることをテスト
+     * - ストレージに存在しないプロパティに対してバリデータを取得
+     * - 空配列が返されることを確認
+     */
+    #[Test]
+    public function returnsEmptyArrayForNonExistentProperty(): void
+    {
+        // Arrange
+        $refClass = new ReflectionClass(TestClassWithValidators::class);
+        $storage = FieldValidatorStorage::createFromClass($refClass);
+        $nonExistentProperty = new ReflectionProperty(TestClassWithDifferentProperties::class, 'email');
+
+        // Act
+        $validators = $storage->getValidatorsForProperty($nonExistentProperty);
+
+        // Assert
+        $this->assertEmpty($validators);
+    }
+}
+
+/**
+ * テスト用クラス：バリデータを持つクラス
+ */
+class TestClassWithValidators
+{
+    public string $name;
+    public int $age;
+
+    #[FieldValidator('name')]
+    public static function validateName(string $value): bool
+    {
+        return strlen($value) > 3;
+    }
+
+    #[FieldValidator('age')]
+    public static function validateAgeMin(int $value): bool
+    {
+        return $value >= 18;
+    }
+
+    #[FieldValidator('age', FunctionalValidatorMode::BEFORE)]
+    public static function validateAgeMax(int $value): bool
+    {
+        return $value <= 120;
+    }
+
+    public static function additionalValidator(string $value): bool
+    {
+        return $value !== 'invalid';
+    }
+}
+
+/**
+ * テスト用クラス：非staticメソッドにアトリビュートが付いているクラス
+ */
+class TestClassWithNonStaticValidator
+{
+    public string $name;
+
+    #[FieldValidator('name')]
+    public function validateName(string $value): bool
+    {
+        return strlen($value) > 3;
+    }
+}
+
+/**
+ * テスト用クラス：存在しないプロパティ名が指定されているクラス
+ */
+class TestClassWithInvalidPropertyName
+{
+    public string $name;
+
+    #[FieldValidator('nonExistentProperty')]
+    public static function validateNonExistent(string $value): bool
+    {
+        return true;
+    }
+}
+
+/**
+ * テスト用クラス：異なるプロパティを持つクラス
+ */
+class TestClassWithDifferentProperties
+{
+    public string $email;
+}
