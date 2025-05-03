b0d42de103c94413c7e6576962dd71fceb1b20b5
akitanabe <tanabe@determaind.biz>
2025-05-03 22:14:32 +0000

    refactor: TypeHintTypeにARRAYケースを追加

--- src/Enums/TypeHintType.php
+++ src/Enums/TypeHintType.php
@@ -9,6 +9,7 @@ enum TypeHintType: string
     case NONE = 'none';
     case MIXED = 'mixed';
     case OBJECT = 'object';
+    case ARRAY = 'array';
     case INT = 'int';
     case STRING = 'string';
     case FLOAT = 'float';
