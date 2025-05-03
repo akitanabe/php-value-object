3fff9c61cb92babbcd4651c4dd935b2fef576200
akitanabe <tanabe@determaind.biz>
2025-05-03 12:44:34 +0000

    refactor: ValidatorFunctionWrapHandlerの内部ロジックを改善し、currentIndexを削除

--- src/Validators/ValidatorFunctionWrapHandler.php
+++ src/Validators/ValidatorFunctionWrapHandler.php
@@ -11,15 +11,20 @@ use PhpValueObject\Core\Validators\Validatorable;
 
 final class ValidatorFunctionWrapHandler
 {
-    private readonly int $currentIndex;
+    private readonly ?Validatorable $validator;
+    private readonly ArrayIterator $validators;
 
     /**
      * @param ArrayIterator<int,Validatorable> $validators
      */
     public function __construct(
-        private ArrayIterator $validators,
+        ArrayIterator $validators,
     ) {
-        $this->currentIndex = $validators->key() > -1 ? $validators->key() : -1;
+        $this->validators = $validators;
+        $this->validator = $this->validators->current();
+
+        // 次のハンドラーを準備
+        $this->validators->next();
     }
 
     /**
@@ -32,20 +37,14 @@ final class ValidatorFunctionWrapHandler
     public function __invoke(mixed $value): mixed
     {
         // バリデータがない場合は、値をそのまま返す
-        if ($this->currentIndex < 0) {
+        if ($this->validator === null) {
             return $value;
         }
 
-        // 現在のバリデータを取得
-        $this->validators->seek($this->currentIndex);
-        $validator = $this->validators->current();
-
-        // 次のハンドラーを準備
-        $this->validators->next();
         $nextHandler = new self($this->validators);
 
         // バリデータに次のハンドラーを含めて実行を委譲
         // 各バリデータ内部で次のハンドラーを呼び出すかどうかを決定する
-        return $validator->validate($value, $nextHandler);
+        return $this->validator->validate($value, $nextHandler);
     }
 }
