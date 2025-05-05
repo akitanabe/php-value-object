85c0918b5992733e25356bee8a4665ff00632726
akitanabe <tanabe@determaind.biz>
2025-05-05 12:55:07 +0000

    refactor: バリデータの初期化をコンストラクタから遅延ロードメソッドに移動し、コードの可読性を向上

--- src/Validators/ValidatorFunctionWrapHandler.php
+++ src/Validators/ValidatorFunctionWrapHandler.php
@@ -15,25 +15,16 @@ use PhpValueObject\Core\Validators\Validatorable;
  */
 final class ValidatorFunctionWrapHandler
 {
-    private readonly Validatorable $validator;
-
-    private readonly ?self $nextHandler;
-
+    private ?Validatorable $validator = null;
+    private ?self $nextHandler = null;
+    private readonly SplQueue $validatorQueue;
     /**
      * @param validator_queue $validatorQueue
      */
     public function __construct(
         SplQueue $validatorQueue,
     ) {
-        if ($validatorQueue->isEmpty() == false) {
-            $this->validator = $validatorQueue->dequeue();
-            $this->nextHandler = new self($validatorQueue);
-
-        } else {
-            // バリデータキューが空になった場合はIdenticalValidatorを使用して返すのみにする
-            $this->validator = new IdenticalValidator();
-            $this->nextHandler = null;
-        }
+        $this->validatorQueue = $validatorQueue;
 
     }
 
@@ -46,8 +37,31 @@ final class ValidatorFunctionWrapHandler
      */
     public function __invoke(mixed $value): mixed
     {
+        // 実行時にバリデータとnextHandlerを取得する
+        $this->lazyLoadValidator();
+
         // バリデータに次のハンドラーを含めて実行を委譲
         // 各バリデータ内部で次のハンドラーを呼び出すかどうかを決定する
         return $this->validator->validate($value, $this->nextHandler);
+    }
+
+    /**
+     * バリデータを遅延ロードする
+     */
+    private function lazyLoadValidator(): void
+    {
+        // すでにバリデータが設定されている場合は何もしない
+        if ($this->validator !== null) {
+            return;
+        }
+
+        // バリデータキューが空になった場合はIdenticalValidatorを使用して返すのみにする
+        if ($this->validatorQueue->isEmpty()) {
+            $this->validator = new IdenticalValidator();
+            return;
+        }
+
+        $this->validator = $this->validatorQueue->dequeue();
+        $this->nextHandler = new self($this->validatorQueue);
     }
 }
